3664da61a1983bbbe1d6d01a4221f69cc677b74a
Luke Przekwas <galactica@The-Galactica.local>
2018-01-22 14:14:21 -0600

    added custom bird

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.gif';
+import '../App.css';
 import AddChirp from './AddChirp';
 
 class App extends Component {
@@ -11,7 +11,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Chirper</h1>
         </header>
+
         <AddChirp />
+
       </div>
     );
   }
